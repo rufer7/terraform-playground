ff495e3f71ff500da19b956d57402c9526848db0
Marc Rufer <marc.rufer@isolutions.ch>
2025-10-19 23:14:16 +0200

    Fix execution error

--- iac/main-appsrv.tf
+++ iac/main-appsrv.tf
@@ -27,10 +27,6 @@ resource "azurerm_linux_web_app" "appsrv" {
   app_settings = merge(
     {
       "WEBSITE_RUN_FROM_PACKAGE" = "1"
-      "AzureAd__ClientId"        = azuread_application.aadapp.client_id
-      "AzureAd__Domain"          = data.azuread_domains.aad_domains.domains[0].domain_name
-      "AzureAd__TenantId"        = var.tenant_id
-      "AzureKeyVaultEndpoint"    = azurerm_key_vault.kv.vault_uri
     }
   )
   lifecycle {
